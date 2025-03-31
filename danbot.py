80aee27706d86d3c5c2a71a168f0f2dfe2e1f39a
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2025-03-31 15:53:54 +0100

    Update danbot.py

--- danbot.py
+++ danbot.py
@@ -18,12 +18,13 @@ def get_random_wisdom(wisdom_list):
 wisdom_list = load_wisdom('wisdom.txt')
 
 # Streamlit app layout
-st.sidebar.image("arinet.png", width=180)
+st.sidebar.image("arinet.png", width=100)
 #st.subheader("DanPilot")
 st.image("danpilot.png")
 st.sidebar.write("Remember - there's no Gain without Payne. \
          Harnessing the power of neural networks and mind downloading,  \
          AI is paving the way for the future. ")
+st.sidebar.image("arinet.png", width=100)
 st.write("Ask me anything:")
 
 # User input
