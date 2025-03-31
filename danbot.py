579b29e246b5f2d479b22c2ff42a250da739479e
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2025-03-31 16:11:26 +0100

    Update danbot.py

--- danbot.py
+++ danbot.py
@@ -38,11 +38,11 @@ text5 = "> reinforcing neural cortex algorithm...."
 st.sidebar.markdown(f"<p style='font-family:Courier New;'>{text}</p>", unsafe_allow_html=True)
 time.sleep(1)
 st.sidebar.markdown(f"<p style='font-family:Courier New;'>{text2}</p>", unsafe_allow_html=True)
-time.sleep(1)
+time.sleep(2)
 st.sidebar.markdown(f"<p style='font-family:Courier New;'>{text3}</p>", unsafe_allow_html=True)
 time.sleep(1)
 st.sidebar.markdown(f"<p style='font-family:Courier New;'>{text4}</p>", unsafe_allow_html=True)
-time.sleep(1)
+time.sleep(2)
 st.sidebar.markdown(f"<p style='font-family:Courier New;'>{text5}</p>", unsafe_allow_html=True)
 
 st.write("Ask me anything:")
